61370eea5b027f5f49f7123508e5ba5479a5ca4c
Mishkovec <neilsa@mail.ru>
2019-03-04 21:51:29 +0300

    add code money exchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,57 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    var num = currency;
+    var obj = {};
+    if(num<=0){
+       return obj;
+    }
+    if (num>10000){
+        obj.error = "You are rich, my friend! We don't have so much coins for exchange";
+        //return obj.error;
+    } else{
+    if(num>=50){
+       obj["H"]=count(num, 50); 
+       num = num - obj["H"]*50;
+       }
+    
+    if(num>=25){
+       obj["Q"]=count(num, 25); 
+       num = num - obj["Q"]*25;
+       }
+    
+    if(num>=10){
+       obj["D"]=count(num, 10); 
+       num = num - obj["D"]*10;
+       }
+    
+    if(num>=5){
+       obj["N"]=count(num, 5);
+       num = num - obj["N"]*5;
+       }
+    
+    if(num>=1){
+       obj["P"]=count(num, 1); 
+       num = num - obj["P"]*1;
+       }
+    }
+  var str="";
+    for(var key in obj){
+       str+=key +":"+ obj[key]+",";
+       }
+    //return str;
+    
+    //return {"H":obj["H"], "Q":obj["Q"], "D":obj["D"], "N":obj["N"], "P":obj["P"]};
+    //return obj;
+    return JSON.stringify(obj);
+}
+
+function count(num, sub){
+       var amount=0;
+        while(num>=sub){
+            num-=sub;
+            amount++;
+        }
+        
+    return amount ;
+}
 }
